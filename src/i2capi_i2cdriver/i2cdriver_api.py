539fb26421dbebe7e74a524fa533b5cda036bacb
Dmitri Priimak <priimak@gmail.com>
2026-08-12 17:48:41 -0500

    Log messages are called from under I2CMessage

--- src/i2capi_i2cdriver/i2cdriver_api.py
+++ src/i2capi_i2cdriver/i2cdriver_api.py
@@ -1,8 +1,6 @@
-from typing import Optional
-
-from bitstring import Bits, BitArray
-from i2c_api import I2CMaster, I2CLogger
-from i2c_api.log import START, I2CTransactionElement, DATA_MOSI, WRITE, ACK, NACK, STOP, READ, DATA_MISO, RESTART
+from bitstring import BitArray, Bits
+from i2c_api import I2CLogger, I2CMaster, I2CMessage
+from i2c_api.log import I2CTransactionElement
 from i2cdriver import I2CDriver
 
 
@@ -10,94 +8,110 @@ class DummyI2CLogger(I2CLogger):
     def log_message(self, message: list[I2CTransactionElement]):
         pass
 
+
 class I2CMasterI2CDriver(I2CMaster):
     def __init__(self, driver: I2CDriver, logger: I2CLogger | None = None) -> None:
         self.driver = driver
-        self._pullup_codes = ["disabled", "2.2K", "4.3K", "1.5K", "4.7K", "1.5K", "2.2K", "1.1K"]
+        self._pullup_codes = [
+            "disabled",
+            "2.2K",
+            "4.3K",
+            "1.5K",
+            "4.7K",
+            "1.5K",
+            "2.2K",
+            "1.1K",
+        ]
         self._pullup_values = ["disabled", "4.7K", "4.3K", "2.2K", "1.5K", "1.1K"]
         self.__logger = DummyI2CLogger() if logger is None else logger
 
     def logger(self) -> I2CLogger:
         return self.__logger
 
-    def write(self, address: int, data: Bits | str | int | list[int], num_bytes: int | None = None) -> bool:
+    def write(
+        self,
+        address: int,
+        data: Bits | str | int | list[int],
+        num_bytes: int | None = None,
+    ) -> bool:
         log_msg = []
         payload = I2CMaster.pad_payload(I2CMaster.mk_payload(data), num_bytes)
         try:
-            log_msg.append(START)
-            log_msg.append(DATA_MOSI(BitArray(f"uint:7={address}")))
-            log_msg.append(WRITE)
+            log_msg.append(I2CMessage.START)
+            log_msg.append(I2CMessage.DATA_MOSI(BitArray(f"uint:7={address}")))
+            log_msg.append(I2CMessage.WRITE)
             if not self.driver.start(address, 0):
-                log_msg.append(NACK)
+                log_msg.append(I2CMessage.NACK)
                 return False
-            log_msg.append(ACK)
-            log_msg.append(DATA_MOSI(payload))
+            log_msg.append(I2CMessage.ACK)
+            log_msg.append(I2CMessage.DATA_MOSI(payload))
             if self.driver.write(payload.bytes):
-                log_msg.append(ACK)
+                log_msg.append(I2CMessage.ACK)
                 return True
             else:
-                log_msg.append(NACK)
+                log_msg.append(I2CMessage.NACK)
                 return False
         finally:
             self.driver.stop()
-            log_msg.append(STOP)
+            log_msg.append(I2CMessage.STOP)
             self.__logger.log_message(log_msg)
 
-    def read(self, address: int, num_bytes: int = 1) -> Optional[Bits]:
+    def read(self, address: int, num_bytes: int = 1) -> Bits | None:
         log_msg = []
         try:
-            log_msg.append(START)
-            log_msg.append(DATA_MOSI(BitArray(f"uint:7={address}")))
-            log_msg.append(READ)
+            log_msg.append(I2CMessage.START)
+            log_msg.append(I2CMessage.DATA_MOSI(BitArray(f"uint:7={address}")))
+            log_msg.append(I2CMessage.READ)
             if not self.driver.start(address, 1):
-                log_msg.append(NACK)
+                log_msg.append(I2CMessage.NACK)
                 return None
             else:
-                log_msg.append(ACK)
+                log_msg.append(I2CMessage.ACK)
                 data_from_the_client = Bits(self.driver.read(num_bytes))
-                log_msg.append(DATA_MISO(data_from_the_client))
+                log_msg.append(I2CMessage.DATA_MISO(data_from_the_client))
                 return data_from_the_client
         finally:
             self.driver.stop()
-            log_msg.append(STOP)
+            log_msg.append(I2CMessage.STOP)
             self.__logger.log_message(log_msg)
 
-    def read_register(self, address: int, register: int, num_bytes: int = 1, use_restart: bool = False) \
-            -> Optional[Bits]:
+    def read_register(
+        self, address: int, register: int, num_bytes: int = 1, use_restart: bool = False
+    ) -> Bits | None:
         log_msg = []
         try:
-            log_msg.append(START)
-            log_msg.append(DATA_MOSI(BitArray(f"uint:7={address}")))
-            log_msg.append(WRITE)
+            log_msg.append(I2CMessage.START)
+            log_msg.append(I2CMessage.DATA_MOSI(BitArray(f"uint:7={address}")))
+            log_msg.append(I2CMessage.WRITE)
             if not self.driver.start(address, 0):
-                log_msg.append(NACK)
+                log_msg.append(I2CMessage.NACK)
                 return None
 
-            log_msg.append(ACK)
+            log_msg.append(I2CMessage.ACK)
             payload = BitArray(f"uint:8={register}")
-            log_msg.append(DATA_MOSI(payload))
+            log_msg.append(I2CMessage.DATA_MOSI(payload))
             if not self.driver.write(payload.bytes):
-                log_msg.append(NACK)
+                log_msg.append(I2CMessage.NACK)
                 return None
-            log_msg.append(ACK)
+            log_msg.append(I2CMessage.ACK)
             if not use_restart:
                 self.driver.stop()
-                log_msg.append(STOP)
-                log_msg.append(START)
+                log_msg.append(I2CMessage.STOP)
+                log_msg.append(I2CMessage.START)
             else:
-                log_msg.append(RESTART)
+                log_msg.append(I2CMessage.RESTART)
 
             if not self.driver.start(address, 1):
-                log_msg.append(NACK)
+                log_msg.append(I2CMessage.NACK)
                 return None
             else:
-                log_msg.append(ACK)
+                log_msg.append(I2CMessage.ACK)
             data_from_the_client = Bits(self.driver.read(num_bytes))
-            log_msg.append(DATA_MISO(data_from_the_client))
+            log_msg.append(I2CMessage.DATA_MISO(data_from_the_client))
             return data_from_the_client
         finally:
             self.driver.stop()
-            log_msg.append(STOP)
+            log_msg.append(I2CMessage.STOP)
             self.__logger.log_message(log_msg)
 
     def scan(self) -> list[int]:
@@ -123,4 +137,6 @@ class I2CMasterI2CDriver(I2CMaster):
         if speed in self.list_clk_speeds():
             self.driver.setspeed(speed)
         else:
-            raise RuntimeError("Invalid clock speed value. Only 100 and 400 are allowed for this device.")
+            raise RuntimeError(
+                "Invalid clock speed value. Only 100 and 400 are allowed for this device."
+            )
