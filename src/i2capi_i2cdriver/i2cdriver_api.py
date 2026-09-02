0bd55129e728199b88ff86a92ee557e20b532b93
Dmitri Priimak <priimak@gmail.com>
2026-09-02 13:44:22 -0500

    Added code to support I2C language

--- src/i2capi_i2cdriver/i2cdriver_api.py
+++ src/i2capi_i2cdriver/i2cdriver_api.py
@@ -1,7 +1,10 @@
+from functools import partial, reduce
 from typing import override
 
 from bitstring import BitArray, Bits
 from i2c_api import I2CError, I2CLogger, I2CMaster, I2CMessage, RegisterAddress
+from i2c_api.commands import Address, Data, P, Read, S, Sr, W
+from i2c_api.language import I2CTransaction
 from i2c_api.log import I2CTransactionElement
 from i2cdriver import I2CDriver
 
@@ -140,6 +143,80 @@ class I2CMasterI2CDriver(I2CMaster):
             if end_with_stop:
                 self.driver.stop()
                 log_msg.append(I2CMessage.STOP)
+
+    @override
+    def _exec(self, transaction: I2CTransaction) -> tuple[list[list[BitArray]], bool]:
+        calls = []
+        data_out = []
+        data_rsp = []
+        nack = []
+        stop_called = []
+        f = None
+
+        def read(num_bytes: int) -> None:
+            data_from_the_client = Bits(self.driver.read(num_bytes))
+            bdata = [BitArray(f"uint:8={x}") for x in data_from_the_client.tobytes()]
+            data_rsp.append(bdata)
+
+        def start(address: int, write_flag: int) -> None:
+            if not self.driver.start(address, write_flag):
+                nack.append(True)
+
+        def write(data_bytes: bytes) -> None:
+            if not self.driver.write(data_bytes):
+                nack.append(True)
+
+        def build_write_call():
+            if data_out != []:
+                wdata = reduce(
+                    lambda acc, b: acc + b,
+                    [BitArray(f"uint:8={d}") for d in data_out],
+                    BitArray(),
+                )
+                calls.append(partial(write, wdata.bytes))
+                data_out.clear()
+
+        def stop():
+            stop_called.append(True)
+            self.driver.stop()
+
+        for cmd in transaction._i2c_commands:
+            if nack != []:
+                break
+            match cmd:
+                case S():
+                    f = partial(start)
+                case Address(address):
+                    f = partial(f, address)
+                case W():
+                    f = partial(f, 0)
+                    calls.append(f)
+                    f = None
+                case Read(num_bytes):
+                    f = partial(f, 1)
+                    calls.append(f)
+                    f = None
+                    calls.append(partial(read, num_bytes))
+                case Sr():
+                    build_write_call()
+                    f = partial(start)
+                case P():
+                    build_write_call()
+                    calls.append(stop)
+                case Data(data):
+                    data_out.append(data)
+
+        try:
+            for i, c in enumerate(calls):
+                if nack == []:
+                    c()
+                else:
+                    break
+
+            return [[c.uint for c in d] for d in data_rsp], (nack == [])
+        finally:
+            if stop_called != []:
+                self.driver.stop()
 
     @override
     def write_register(
